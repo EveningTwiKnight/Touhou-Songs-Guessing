415c6d7956ebc632f8dee9fa45672e911909a1bf
Twilight Knight <dy33yanxu@126.com>
2026-08-15 22:39:34 +0800

    Add files via upload

---
+++ song_database.js
@@ -0,0 +1,17 @@
+const song_database = {
+    Scarlet_Devil:[ //红魔乡
+        { name: '亡き王女の為のセプテット', file: 'audio/hong/wangnv.mp3' },
+        { name: '妖魔夜行', file: 'audio/hong/yaomo.mp3' },
+        { name: 'おてんば恋娘', file: 'audio/hong/lianniang.mp3' },
+    ],
+    Perfect_Cherry: [  // 妖妖梦
+        { name: '妖々夢 ～ Snow or Cherry', file: 'audio/yao/yaoyaomeng.mp3' },
+        { name: '無何有の郷 ～ Deep Mountain', file: 'audio/yao/wuheyou.mp3' },
+        { name: 'クリスタライズシルバー', file: 'audio/yao/crystal.mp3' },
+    ],
+    Imperishable_Night: [ // 永夜抄
+        { name: '永夜抄 ～ Eastern Night', file: 'audio/yong/yongye.mp3' },
+        { name: '幻視の夜 ～ Ghostly Eyes', file: 'audio/yong/huanshi.mp3' },
+        { name: '竹取飛翔 ～ Lunatic Princess', file: 'audio/yong/zhuyin.mp3' },
+    ]
+};
